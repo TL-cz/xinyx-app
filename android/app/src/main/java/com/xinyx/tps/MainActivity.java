954ae201d2706fa0dca0ea8260a5aaa9f2478064
TL-cz <3225985132@qq.com>
2026-09-11 18:14:19 +0800

    Update MainActivity.java

--- android/app/src/main/java/com/xinyx/tps/MainActivity.java
+++ android/app/src/main/java/com/xinyx/tps/MainActivity.java
@@ -1,5 +1,12 @@
 package com.xinyx.tps;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bridge.getWebView().loadUrl("http://8.134.192.185:4000");
+    }
+}
